3526c0f737ce183b5a28ce6de0f21d6ec64337dd
Angel0973180707 <a0973180707@gmail.com>
2026-01-08 21:08:18 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,10 @@
-const CACHE_NAME = 'angel-breath-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './script.js',
-  './manifest.json',
-  'https://img.icons8.com/ios-filled/192/F2D388/clover.png',
-  'https://img.icons8.com/ios-filled/512/F2D388/clover.png'
-];
+const CACHE_NAME = 'angel-v2';
+const ASSETS = ['./', './index.html', './script.js', './manifest.json'];
 
-// 安裝 Service Worker 並快取檔案
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-// 啟動 Service Worker 並清理舊快取
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
-
-// 攔截請求並優先從快取讀取
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
